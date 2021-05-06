613a334f058a019c139cb7c613f5e4f409350f00
Adan Moses <adanmoses@pm.me>
2021-05-06 20:01:43 +1000

    working on removing clicking after winning and showing who's player's turn it is

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // create main loop --done
 // turns, inside event listener function or outside of it?
 // -----------
-// add Restart function
+// add Restart function -- done
 // add scoreboard
 // if there's a winner you can't click anymore
 
@@ -11,8 +11,11 @@ const xoBoxes = document.querySelectorAll(".board div");
 const boxesArray = Array.from(xoBoxes);
 const xPlayer = "X";
 const oPlayer = "O";
-let currentPlayer = xPlayer;
+let currentPlayer = oPlayer;
 let clickCounter = 0;
+
+const playerTurn = document.querySelector("#player-turn");
+playerTurn.textContent = `${currentPlayer} Player Turn`;
 
 for (let i = 0; i < xoBoxes.length; i++) {
 	let currentBox = xoBoxes[i];
@@ -23,6 +26,7 @@ for (let i = 0; i < xoBoxes.length; i++) {
 	function clickEvent() {
 		currentPlayer = currentPlayer === oPlayer ? xPlayer : oPlayer;
 		currentBox.textContent = currentPlayer;
+		playerTurn.textContent = `${currentPlayer} Player Turn`;
 		winningCombinations();
 		clickCounter++;
 		if (clickCounter == 9) {
@@ -35,60 +39,72 @@ for (let i = 0; i < xoBoxes.length; i++) {
 		for (let i = 0; i < boxesArray.length; i++) {
 			boxesArray[i].textContent = " ";
 		}
+		clickCounter = 0;
+	}
+
+	function removeClick() {
+		for (let i = 0; i < xoBoxes.length; i++) {
+			xoBoxes[i].removeEventListener("click", clickEvent);
+		}
+	}
+
+	function winningCombinations() {
+		if (
+			boxesArray[0].textContent === currentPlayer &&
+			boxesArray[1].textContent === currentPlayer &&
+			boxesArray[2].textContent === currentPlayer
+		) {
+			alert(`${currentPlayer} Player Wins!`);
+			removeClick();
+		} else if (
+			boxesArray[3].textContent === currentPlayer &&
+			boxesArray[4].textContent === currentPlayer &&
+			boxesArray[5].textContent === currentPlayer
+		) {
+			alert(`${currentPlayer} Player Wins!`);
+			removeClick();
+		} else if (
+			boxesArray[6].textContent === currentPlayer &&
+			boxesArray[7].textContent === currentPlayer &&
+			boxesArray[8].textContent === currentPlayer
+		) {
+			alert(`${currentPlayer} Player Wins!`);
+			removeClick();
+		} else if (
+			boxesArray[0].textContent === currentPlayer &&
+			boxesArray[3].textContent === currentPlayer &&
+			boxesArray[6].textContent === currentPlayer
+		) {
+			alert(`${currentPlayer} Player Wins!`);
+			removeClick();
+		} else if (
+			boxesArray[1].textContent === currentPlayer &&
+			boxesArray[4].textContent === currentPlayer &&
+			boxesArray[7].textContent === currentPlayer
+		) {
+			alert(`${currentPlayer} Player Wins!`);
+			removeClick();
+		} else if (
+			boxesArray[2].textContent === currentPlayer &&
+			boxesArray[5].textContent === currentPlayer &&
+			boxesArray[8].textContent === currentPlayer
+		) {
+			alert(`${currentPlayer} Player Wins!`);
+			removeClick();
+		} else if (
+			boxesArray[0].textContent === currentPlayer &&
+			boxesArray[4].textContent === currentPlayer &&
+			boxesArray[8].textContent === currentPlayer
+		) {
+			alert(`${currentPlayer} Player Wins!`);
+			removeClick();
+		} else if (
+			boxesArray[2].textContent === currentPlayer &&
+			boxesArray[4].textContent === currentPlayer &&
+			boxesArray[6].textContent === currentPlayer
+		) {
+			alert(`${currentPlayer} Player Wins!`);
+			removeClick();
+		}
 	}
 }
-
-function winningCombinations() {
-	if (
-		boxesArray[0].textContent === currentPlayer &&
-		boxesArray[1].textContent === currentPlayer &&
-		boxesArray[2].textContent === currentPlayer
-	) {
-		alert(`${currentPlayer} Player Wins!`);
-	} else if (
-		boxesArray[3].textContent === currentPlayer &&
-		boxesArray[4].textContent === currentPlayer &&
-		boxesArray[5].textContent === currentPlayer
-	) {
-		alert(`${currentPlayer} Player Wins!`);
-	} else if (
-		boxesArray[6].textContent === currentPlayer &&
-		boxesArray[7].textContent === currentPlayer &&
-		boxesArray[8].textContent === currentPlayer
-	) {
-		alert(`${currentPlayer} Player Wins!`);
-	} else if (
-		boxesArray[0].textContent === currentPlayer &&
-		boxesArray[3].textContent === currentPlayer &&
-		boxesArray[6].textContent === currentPlayer
-	) {
-		alert(`${currentPlayer} Player Wins!`);
-	} else if (
-		boxesArray[1].textContent === currentPlayer &&
-		boxesArray[4].textContent === currentPlayer &&
-		boxesArray[7].textContent === currentPlayer
-	) {
-		alert(`${currentPlayer} Player Wins!`);
-	} else if (
-		boxesArray[2].textContent === currentPlayer &&
-		boxesArray[5].textContent === currentPlayer &&
-		boxesArray[8].textContent === currentPlayer
-	) {
-		alert(`${currentPlayer} Player Wins!`);
-	} else if (
-		boxesArray[0].textContent === currentPlayer &&
-		boxesArray[4].textContent === currentPlayer &&
-		boxesArray[8].textContent === currentPlayer
-	) {
-		alert(`${currentPlayer} Player Wins!`);
-	} else if (
-		boxesArray[2].textContent === currentPlayer &&
-		boxesArray[4].textContent === currentPlayer &&
-		boxesArray[6].textContent === currentPlayer
-	) {
-		alert(`${currentPlayer} Player Wins!`);
-	}
-}
-// function gameStart () {
-
-// }
