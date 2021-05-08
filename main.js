bf58b204337840ac5dfc9adaad7805023ce9e8c8
Adan Moses <adanmoses@pm.me>
2021-05-08 11:11:34 +1000

    added some html and css, click not allowed after winning anymore

--- main.js
+++ main.js
@@ -5,32 +5,36 @@
 // -----------
 // add Restart function -- done
 // add scoreboard
-// if there's a winner you can't click anymore
+// if there's a winner you can't click anymore --done
 
 const xoBoxes = document.querySelectorAll(".board div");
 const boxesArray = Array.from(xoBoxes);
 const xPlayer = "X";
 const oPlayer = "O";
+let gameActive = true;
 let currentPlayer = oPlayer;
 let clickCounter = 0;
 
 const playerTurn = document.querySelector("#player-turn");
 playerTurn.textContent = `${currentPlayer} Player Turn`;
 
-for (let i = 0; i < xoBoxes.length; i++) {
-	let currentBox = xoBoxes[i];
+for (let i = 0; i < boxesArray.length; i++) {
+	let currentBox = boxesArray[i];
 	const restartBtn = document.querySelector("#restart-btn");
+
 	restartBtn.addEventListener("click", restartGame);
 	currentBox.addEventListener("click", clickEvent, { once: true });
 
 	function clickEvent() {
-		currentPlayer = currentPlayer === oPlayer ? xPlayer : oPlayer;
-		currentBox.textContent = currentPlayer;
-		playerTurn.textContent = `${currentPlayer} Player Turn`;
-		winningCombinations();
-		clickCounter++;
-		if (clickCounter == 9) {
-			alert("It's a draw!");
+		if (gameActive) {
+			currentPlayer = currentPlayer === oPlayer ? xPlayer : oPlayer;
+			currentBox.textContent = currentPlayer;
+			playerTurn.textContent = `${currentPlayer} Player Turn`;
+			winningCombinations();
+			clickCounter++;
+			if (gameActive == true && clickCounter == 9) {
+				alert("It's a draw!");
+			}
 		}
 	}
 
@@ -40,71 +44,66 @@ for (let i = 0; i < xoBoxes.length; i++) {
 			boxesArray[i].textContent = " ";
 		}
 		clickCounter = 0;
+		return (gameActive = true);
 	}
+}
 
-	function removeClick() {
-		for (let i = 0; i < xoBoxes.length; i++) {
-			xoBoxes[i].removeEventListener("click", clickEvent);
-		}
-	}
-
-	function winningCombinations() {
-		if (
-			boxesArray[0].textContent === currentPlayer &&
-			boxesArray[1].textContent === currentPlayer &&
-			boxesArray[2].textContent === currentPlayer
-		) {
-			alert(`${currentPlayer} Player Wins!`);
-			removeClick();
-		} else if (
-			boxesArray[3].textContent === currentPlayer &&
-			boxesArray[4].textContent === currentPlayer &&
-			boxesArray[5].textContent === currentPlayer
-		) {
-			alert(`${currentPlayer} Player Wins!`);
-			removeClick();
-		} else if (
-			boxesArray[6].textContent === currentPlayer &&
-			boxesArray[7].textContent === currentPlayer &&
-			boxesArray[8].textContent === currentPlayer
-		) {
-			alert(`${currentPlayer} Player Wins!`);
-			removeClick();
-		} else if (
-			boxesArray[0].textContent === currentPlayer &&
-			boxesArray[3].textContent === currentPlayer &&
-			boxesArray[6].textContent === currentPlayer
-		) {
-			alert(`${currentPlayer} Player Wins!`);
-			removeClick();
-		} else if (
-			boxesArray[1].textContent === currentPlayer &&
-			boxesArray[4].textContent === currentPlayer &&
-			boxesArray[7].textContent === currentPlayer
-		) {
-			alert(`${currentPlayer} Player Wins!`);
-			removeClick();
-		} else if (
-			boxesArray[2].textContent === currentPlayer &&
-			boxesArray[5].textContent === currentPlayer &&
-			boxesArray[8].textContent === currentPlayer
-		) {
-			alert(`${currentPlayer} Player Wins!`);
-			removeClick();
-		} else if (
-			boxesArray[0].textContent === currentPlayer &&
-			boxesArray[4].textContent === currentPlayer &&
-			boxesArray[8].textContent === currentPlayer
-		) {
-			alert(`${currentPlayer} Player Wins!`);
-			removeClick();
-		} else if (
-			boxesArray[2].textContent === currentPlayer &&
-			boxesArray[4].textContent === currentPlayer &&
-			boxesArray[6].textContent === currentPlayer
-		) {
-			alert(`${currentPlayer} Player Wins!`);
-			removeClick();
-		}
+function winningCombinations() {
+	if (
+		boxesArray[0].textContent === currentPlayer &&
+		boxesArray[1].textContent === currentPlayer &&
+		boxesArray[2].textContent === currentPlayer
+	) {
+		alert(`${currentPlayer} Player Wins!`);
+		return (gameActive = false);
+	} else if (
+		boxesArray[3].textContent === currentPlayer &&
+		boxesArray[4].textContent === currentPlayer &&
+		boxesArray[5].textContent === currentPlayer
+	) {
+		alert(`${currentPlayer} Player Wins!`);
+		return (gameActive = false);
+	} else if (
+		boxesArray[6].textContent === currentPlayer &&
+		boxesArray[7].textContent === currentPlayer &&
+		boxesArray[8].textContent === currentPlayer
+	) {
+		alert(`${currentPlayer} Player Wins!`);
+		return (gameActive = false);
+	} else if (
+		boxesArray[0].textContent === currentPlayer &&
+		boxesArray[3].textContent === currentPlayer &&
+		boxesArray[6].textContent === currentPlayer
+	) {
+		alert(`${currentPlayer} Player Wins!`);
+		return (gameActive = false);
+	} else if (
+		boxesArray[1].textContent === currentPlayer &&
+		boxesArray[4].textContent === currentPlayer &&
+		boxesArray[7].textContent === currentPlayer
+	) {
+		alert(`${currentPlayer} Player Wins!`);
+		return (gameActive = false);
+	} else if (
+		boxesArray[2].textContent === currentPlayer &&
+		boxesArray[5].textContent === currentPlayer &&
+		boxesArray[8].textContent === currentPlayer
+	) {
+		alert(`${currentPlayer} Player Wins!`);
+		return (gameActive = false);
+	} else if (
+		boxesArray[0].textContent === currentPlayer &&
+		boxesArray[4].textContent === currentPlayer &&
+		boxesArray[8].textContent === currentPlayer
+	) {
+		alert(`${currentPlayer} Player Wins!`);
+		return (gameActive = false);
+	} else if (
+		boxesArray[2].textContent === currentPlayer &&
+		boxesArray[4].textContent === currentPlayer &&
+		boxesArray[6].textContent === currentPlayer
+	) {
+		alert(`${currentPlayer} Player Wins!`);
+		return (gameActive = false);
 	}
 }
